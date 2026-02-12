13f5c15bf3249cd1db28a73e32d255b2098df7f6
JacobCPU12 <s283209@uos.ac.uk>
2026-02-12 11:24:33 +0000

    removed existing models in prep for database

--- pacapp/models.py
+++ pacapp/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class PersonalAcademicCoach(models.Model):
-    first_name = models.CharField(max_length=100),
-    last_name = models.CharField(max_length=100),
-    email = models.EmailField(),
-    office_id = models.IntegerField(),
-    specialization_id = models.IntegerField()
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-class Student(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    office_id = models.IntegerField()
-    enrolment_date = models.DateField()
-    assigned_pac_id = models.IntegerField()
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
