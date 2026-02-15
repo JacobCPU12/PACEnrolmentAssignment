9f9f5fe63b15c55ae01d698eb93e410399c18cb0
JacobCPU12 <s283209@uos.ac.uk>
2026-02-15 14:30:12 +0000

    Modified ALLOWED_HOSTS again (please just work)

--- pacsystem/settings.py
+++ pacsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gu4&zs4_orjnb44ox5i^&*$1_1m9b-!$0t38vk_-0%+n--utgk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://pacenrolmentassignment.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["pacenrolmentassignment.onrender.com", "127.0.0.1"]
 
 
 # Application definition
