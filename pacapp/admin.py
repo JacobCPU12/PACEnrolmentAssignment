5b2959d8878e649f22ab908de94e76ffcfe138d6
apalo7 <s309918@uos.ac.uk>
2026-02-17 10:15:52 +0000

    Update admin.py

--- pacapp/admin.py
+++ pacapp/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from models import Offices
-from models import Specializations
 from models import Pacs
 from models import Students
 from models import PastoralNotes
 
 # Register your models here.
 
-admin.site.register(Offices)
-admin.site.register(Specializations)
 admin.site.register(Pacs)
 admin.site.register(Students)
 admin.site.register(PastoralNotes)
-
