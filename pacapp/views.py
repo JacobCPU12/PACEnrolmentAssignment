5ae4c123f5391b69ba8fd2765ee432b82856f1d1
apalo7 <s309918@uos.ac.uk>
2026-02-17 10:15:23 +0000

    Update views.py

--- pacapp/views.py
+++ pacapp/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render
-from models import Offices
-from models import Specializations
 from models import Pacs
 from models import Students
 from models import PastoralNotes
 
 # Create your views here.
 
-def myoffices(request):
-    myoffices = Offices.objects.all()
-    context = {'myoffices': myoffices}
-    return render(request, 'offices.html', context)
-
-def mypecializations(request):
-    myspecializations = Specializations.objects.all()
-    context = {'myspecializations': myspecializations}
-    return render(request, 'specializations.html', context)
 
 def mypacs(request):
     mypacs = Pacs.objects.all()
