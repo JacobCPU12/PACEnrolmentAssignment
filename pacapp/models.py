ef3a518aef22e811c789a88ba63fd38fea04d48b
apalo7 <s309918@uos.ac.uk>
2026-02-20 15:10:38 +0000

    Update models.py

--- pacapp/models.py
+++ pacapp/models.py
@@ -24,11 +24,6 @@ class Students(models.Model):
         return f'{self.first_name} {self.last_name} {self.student_id}'
 
 
-class PastoralNotes(models.Model):
-    note = models.TextField()
-    student = models.ForeignKey(Students, on_delete=models.CASCADE)
-    pac = models.ForeignKey(Pacs, on_delete=models.CASCADE)
-    date_created = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f'Note for{self.student_id}'
